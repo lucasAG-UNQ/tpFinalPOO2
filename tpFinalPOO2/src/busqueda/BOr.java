a6f49e5249cc52639353bcde59b9d260ff02c181
Luly <Luly@192.168.0.16>
2022-06-12 23:55:56 -0300

    otros métodos de busqueda

---
+++ tpFinalPOO2/src/busqueda/BOr.java
@@ -0,0 +1,26 @@
+package busqueda;
+
+import java.util.List;
+
+import muestra.Muestra;
+
+public class BOr extends BusquedaCompleta{
+	private BusquedaI busqueda1;
+	private BusquedaI busqueda2;
+	
+	public BOr(List<BusquedaI> childBusqueda,BusquedaI busqueda) {
+		super(childBusqueda, busqueda);
+		
+	}
+	
+	    @Override
+	    public void buscarMuestra(List<Muestra> muestras ) {
+		
+		
+		  //this.childBusqueda.forEach();       
+		
+	}
+	
+	
+
+}
---
+++ tpFinalPOO2/src/busqueda/Buscador.java
@@ -0,0 +1,22 @@
+package busqueda;
+
+import java.util.List;
+
+
+import muestra.Muestra;
+
+public class Buscador implements BusquedaI{
+	private List<Muestra> muestras;
+	private BusquedaI busqueda;
+	
+	public Buscador (BusquedaI busqueda, List<Muestra> muestras) {
+		this.muestras=muestras;
+		this.busqueda= busqueda;
+	}
+	
+	@Override
+	public void buscarMuestra(List<Muestra> muestras) {
+		
+	}
+
+}
